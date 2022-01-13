e4582b689a551d0a7ae82b6df2de7d32346047a8
MuhammadJamalAshrafi <33508875+MuhammadJamalAshrafi@users.noreply.github.com>
2022-01-13 23:09:49 +0500

    Add files via upload

---
+++ Assigment3.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<List<String>> friends = [
+    /*A*/ /*B*/ /*C*/ /*D*/
+    /*0*/ ["Ishaaq", "Zubair", "Mehmood", "Basit"],
+    /*1*/ ["Naveed", "Uzair", "Affan", "Bilal"],
+    /*2*/ ["Arbaaz", "Usman", "Zain", "Umair"],
+    /*3*/ ["Ahmed", "Ali", "Yousuf", "Zsama"],
+    /*4*/ ["Naam", "Naam", "Naam", "Naaam"],
+  ];
+
+  print(friends[1][3]);
+  // Excel R=Row, C=Column   :   [R][C]
+}
